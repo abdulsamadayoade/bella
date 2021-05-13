f1d0a20dac2de043db7634812b32e976b24929fc
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-05-13 23:56:22 +0100

    Added Animation

--- js/app.js
+++ js/app.js
@@ -20,3 +20,15 @@ function removeSlideInNavList() {
     leftBox.classList.remove('active');
     rightBox.classList.remove('active');
 }
+
+// FADE IN
+
+var rule = CSSRulePlugin.getRule('.right-box:before');
+
+gsap.to(rule, {cssRule: {scaleY: 0}, duration: 1});
+
+let animRight = gsap.timeline({stagger: 1});
+
+animRight.from('.close', {opacity: 0, y: 10, duration: 1});
+animRight.from('.community', {opacity: 0, y: 10, duration: 1});
+animRight.from('.subscribe', {opacity: 0, y: 10, duration: 1});
