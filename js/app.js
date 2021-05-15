2feb85f0907cf91de200c86b6d1f1e9930853e51
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-05-15 17:04:25 +0100

    Right Nav Animation

--- js/app.js
+++ js/app.js
@@ -13,9 +13,21 @@ function slideInNavList(e) {
     leftBox.classList.add('active');
     rightBox.classList.add('active');
 
+    // LEFT BOX ANIMATION
     gsap.from('.close', {opacity: 0, x: -50, duration: .8, ease: 'back.in(1.7)'});
     gsap.from('.community', {opacity: 0, y: 30, duration: 1, delay: .3, ease: 'back.in(1.7)'});
-    gsap.from('.subscribe', {opacity: 0, y: 20, duration: 1, delay: .7, ease: 'back.in(1.7)'})
+    gsap.from('.subscribe', {opacity: 0, y: 20, duration: 1, delay: .7, ease: 'back.in(1.7)'});
+
+    // RIGHT BOX ANIMATION
+    gsap.from('.leftLink1', {opacity: 0, y: 40, x: -20, duration: .8, delay: .4, ease: 'ease.out(1.5)'});
+    gsap.from('.leftLink2', {opacity: 0, y: 40, x: -20, duration: .8, delay: .8, ease: 'ease.out(1.5)'});
+    gsap.from('.leftLink3', {opacity: 0, y: 40, x: -20, duration: .8, delay: 1.3, ease: 'ease.out(1.5)'});
+    gsap.from('.leftLink4', {opacity: 0, y: 40, x: -20, duration: .8, delay: 1.6, ease: 'ease.out(1.5)'});
+    gsap.from('.leftLink5', {opacity: 0, y: 40, x: -20, duration: .8, delay: 1.9, ease: 'ease.out(1.5)'});
+
+
+    // leftAnim.from('.left-box a', {opacity: 0, y: 40, delay: 2, duration: 1, ease: 'power4.out'});
+    // gsap.from('.omo', {opacity: 0, y: 40, duration: 1, ease: 'power4.out'});
 }
 
 closeMenu.addEventListener('click', removeSlideInNavList);
